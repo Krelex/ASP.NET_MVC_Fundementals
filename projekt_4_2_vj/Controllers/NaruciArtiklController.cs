7fcac9a9b509cf4a7b63f7f5cf18c4c9f8507bdd
Filip Čogelja <filip.cog@gmail.com>
2018-07-19 10:00:31 +0200

    Add files via upload

---
+++ projekt_4_2_vj/Controllers/NaruciArtiklController.cs
@@ -0,0 +1,31 @@
+﻿using projekt_4_2_vj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_4_2_vj.Controllers
+{
+    public class NaruciArtiklController : Controller
+    {
+        // GET: NaruciArtikl
+        public ActionResult naruci()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult naruci(Artikl artikl)
+        {
+            if (artikl.Kolicina > 10)
+            {
+                ViewBag.kolicina = "Trazena kolicna nije dopsutpna na skladistu! :( ";
+                return View("naruci");
+            }
+            else
+            {
+                return View("prikazi", artikl);
+            }
+        }
+    }
+}

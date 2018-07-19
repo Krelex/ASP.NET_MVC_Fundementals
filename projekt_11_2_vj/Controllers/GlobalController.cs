0893c9ab63143d0895d4a4f6c207cae2e266510f
Filip Čogelja <filip.cog@gmail.com>
2018-07-19 10:44:33 +0200

    Add files via upload

---
+++ projekt_11_2_vj/Controllers/GlobalController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_11_2_vj.Controllers
+{
+    public class GlobalController : Controller
+    {
+        // GET: Global
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

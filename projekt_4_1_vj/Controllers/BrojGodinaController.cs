fb175a598651c11e720daca314e51216d4964056
Filip Čogelja <filip.cog@gmail.com>
2018-07-19 09:51:57 +0200

    Add files via upload

---
+++ projekt_4_1_vj/Controllers/BrojGodinaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_4_1_vj.Controllers
+{
+    public class BrojGodinaController : Controller
+    {
+        // GET: BrojGodina
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Racunaj()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Racunaj(DateTime? datum)
+        {
+
+            TimeSpan? total = DateTime.Now - datum;
+            return View(total);
+        }
+    }
+}
---
+++ projekt_4_1_vj/Controllers/TocanOdgovorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_4_1_vj.Controllers
+{
+    public class TocanOdgovorController : Controller
+    {
+        // GET: TocanOdgovor
+        public ActionResult ProvjeriOdgovor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProvjeriOdgovor(string odgovor)
+        {
+            bool tocnost;
+            if (odgovor == "crveni")
+            {
+                tocnost = true;
+            }else
+            {
+                tocnost = false;
+            }
+            return View(tocnost);
+        }
+    }
+}

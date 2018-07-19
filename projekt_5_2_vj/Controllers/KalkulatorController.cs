89afef8a39d32922dece03357cb1f65f0f373ea5
Filip Čogelja <filip.cog@gmail.com>
2018-07-19 10:03:05 +0200

    Add files via upload

---
+++ projekt_5_2_vj/Controllers/KalkulatorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_5_2_vj.Controllers
+{
+    public class KalkulatorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Izracunaj()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Izracunaj(int? prvi , int? drugi , string operacija)
+        {
+            try
+            {
+                if (prvi == null || drugi == null)
+                {
+                    throw new Exception("Niste unjeli ispravana parametar!");
+                }
+                ViewBag.prvi = prvi;
+                ViewBag.drugi = drugi;
+
+                string model = operacija;
+                return View((object)model);
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.error = ex.Message;
+                return View();
+            }
+
+        }
+    }
+}
---
+++ projekt_5_2_vj/Controllers/RandomNumController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_5_2_vj.Controllers
+{
+    public class RandomNumController : Controller
+    {
+        // GET: RandomNum
+        public ActionResult Random()
+        {
+            Random random = new Random();
+
+            int modelNum = random.Next(1, 1000);
+
+            return View(modelNum);
+        }
+    }
+}

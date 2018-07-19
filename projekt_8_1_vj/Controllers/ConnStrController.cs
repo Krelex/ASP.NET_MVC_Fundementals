a59f24eaf461634b02c22d26ec3af7bf490c25f9
Filip Čogelja <filip.cog@gmail.com>
2018-07-19 10:22:10 +0200

    Add files via upload

---
+++ projekt_8_1_vj/Controllers/ConnStrController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace projekt_8_1_vj.Controllers
+{
+    public class ConnStrController : Controller
+    {
+        // GET: ConnStr
+        public string  ConnectionWinAuth()
+        {
+            string winConn = ConfigurationManager.ConnectionStrings["WinConnString"].ConnectionString;
+            string state;
+
+            using (SqlConnection conn = new SqlConnection(winConn) )
+            {
+                conn.Open();
+                state = conn.State.ToString();
+            }
+
+            return "Stanje konekecije na BAZU : " + state;
+        }
+
+        public string ConnectionSQLAuth()
+        {
+            string sqlConn = ConfigurationManager.ConnectionStrings["sqlConnString"].ConnectionString;
+            string state;
+
+            using (SqlConnection conn = new SqlConnection(sqlConn))
+            {
+                conn.Open();
+                state = conn.State.ToString();
+            }
+
+            return "Stanje konekecije na BAZU : " + state;
+        }
+    }
+}

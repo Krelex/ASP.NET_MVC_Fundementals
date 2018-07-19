ca343057c791597c84258d69b389a82e23a498f5
Filip Čogelja <filip.cog@gmail.com>
2018-07-19 10:16:04 +0200

    Add files via upload

---
+++ projekt_7_1_vj/Controllers/OsobaController.cs
@@ -0,0 +1,39 @@
+﻿using projekt_7_1_vj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_7_1_vj.Controllers
+{
+    public class OsobaController : Controller
+    {
+        public ActionResult UnesiOsobu()
+        {
+            return View();
+        }
+
+        public ActionResult ProvjeriOsobu(Osoba osoba)
+        {
+            if (string.IsNullOrWhiteSpace(osoba.Ime))
+            {
+                ModelState.AddModelError("Ime", "Ime je obavezno!");
+            }
+
+            if (string.IsNullOrWhiteSpace(osoba.Prezime))
+            {
+                ModelState.AddModelError("Prezime", "Prezime je obavezno!");
+            }
+            
+            if (ModelState.IsValid)
+            {
+                return View(osoba);
+            }
+            else
+            {
+                return View("UnesiOsobu");
+            }
+        }
+    }
+}
---
+++ projekt_7_1_vj/Models/Osoba.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projekt_7_1_vj.Models
+{
+    public class Osoba
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRod { get; set; }
+        public string Mobitel { get; set; }
+    }
+}

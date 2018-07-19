bbdc284145fddcce80e15cae92133548cca5419c
Filip Čogelja <filip.cog@gmail.com>
2018-07-19 10:10:31 +0200

    Add files via upload

---
+++ projekt_5_4_vj/Controllers/PrikazChildController.cs
@@ -0,0 +1,54 @@
+﻿using projekt_5_4_vj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_5_4_vj.Controllers
+{
+    public class PrikazChildController : Controller
+    {
+        // GET: ParcijalniPogled
+        public ActionResult ObradiListu()
+        {
+            Artikli ar1 = new Artikli()
+            {
+                Kategorija = "Food",
+                Naziv = "Banana",
+                Cijena = 10,
+                Kolicina = 10
+            };
+            Artikli ar2 = new Artikli()
+            {
+                Kategorija = "Food",
+                Naziv = "Apple",
+                Cijena = 20,
+                Kolicina = 3
+            };
+            Artikli ar3 = new Artikli()
+            {
+                Kategorija = "Food",
+                Naziv = "Hamburger",
+                Cijena = 25,
+                Kolicina = 1
+            };
+            Artikli ar4 = new Artikli()
+            {
+                Kategorija = "Car",
+                Naziv = "Golf",
+                Cijena = 1000,
+                Kolicina = 2
+            };
+
+            List<Artikli> repo = new List<Artikli>() { ar1, ar2, ar3, ar4 };
+
+            return View(repo);
+        }
+
+        public string OdrediKategoriju(Artikli artikl)
+        {
+            return artikl.Kategorija;
+        }
+    }
+}
---
+++ projekt_5_4_vj/Models/Artikli.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projekt_5_4_vj.Models
+{
+    public class Artikli
+    {
+        public string Kategorija { get; set; }
+        public string Naziv { get; set; }
+        public decimal Cijena { get; set; }
+        public decimal Kolicina { get; set; }
+
+
+        public string Ukupno()
+        {
+            return (this.Cijena * this.Kolicina).ToString();
+        }
+    }
+}
